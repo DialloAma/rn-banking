abeea78feee96a18d2187639b481c9e9c51b9785
DIALLO <amadou9393@yahoo.fr>
2022-03-15 02:26:40 +0000

    installed the body parser and connect my server to the database on mongoose

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
 const server = express();
-server.listen(4000,"localhost",()=>console.log("server is ready"));
+server.use(bodyparser.json());
+mongoose.connect("mongodb+srv://AdminUser:Ry4AnYuf1dtJgVMJ@cluster0.vkk55.mongodb.net/Banking_App?retryWrites=true&w=majority",
+{useNewUrlParser:true,useUnifiedTopology:true})
+/*.then((res)=>{
+    server.listen(3000,"localhost",()=>console.log("server is ready"));
+}).catch(err=>console.log(err))*/
+mongoose.connection.on("connected",()=>console.log("database connected"));
+mongoose.connection.on("error",(err)=>console.log(err))
+server.listen(3000,"localhost",()=>console.log("server is ready"));
+
